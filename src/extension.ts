11c88e5cd4c3364990a66d80a2f4f1bfbb157fe5
onemore <909413016@qq.com>
2023-02-02 19:39:36 +0800

    起步中

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import * as myluaserver from './myluaserver';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+    myluaserver.activate(context);
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -23,4 +27,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    myluaserver.deactivate();
+}
---
+++ src/myluaserver.ts
@@ -0,0 +1,169 @@
+import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
+import * as vscode from 'vscode';
+import {
+    workspace as Workspace,
+    ExtensionContext,
+    commands as Commands,
+    TextDocument,
+    Uri,
+    window,
+    Disposable,
+} from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    DocumentSelector,
+} from 'vscode-languageclient/node';
+
+// 控制配置。todo 具体怎么用还要看
+function registerCustomCommands(context: ExtensionContext) {
+    context.subscriptions.push(Commands.registerCommand('mylua.config', (changes) => {
+        let propMap: Map<string, Map<string, any>> = new Map();
+        for (const data of changes) {
+            let config = Workspace.getConfiguration(undefined, Uri.parse(data.uri));
+            if (data.action === 'add') {
+                let value: any[] = config.get(data.key) || [];
+                value.push(data.value);
+                config.update(data.key, value, data.global);
+                continue;
+            }
+            if (data.action === 'set') {
+                config.update(data.key, data.value, data.global);
+                continue;
+            }
+            if (data.action === 'prop') {
+                if (!propMap.has(data.key)) {
+                    propMap[data.key] = config.get(data.key);
+                }
+                propMap[data.key][data.prop] = data.value;
+                config.update(data.key, propMap[data.key], data.global);
+                continue;
+            }
+        }
+    }));
+}
+
+
+class MyLuaClient {
+    public client!: LanguageClient;
+    private disposables = new Array<Disposable>();
+
+    constructor(private context: ExtensionContext,
+        private documentSelector: DocumentSelector) {
+    }
+
+    async start() {
+        // Options to control the language client
+        let clientOptions: LanguageClientOptions = {
+            // Register the server for plain text documents
+            documentSelector: this.documentSelector,
+            progressOnInitialization: true,
+            markdown: {
+                isTrusted: true,
+            },
+            initializationOptions: {
+                changeConfiguration: true,
+            }
+        };
+
+        let config = Workspace.getConfiguration(undefined, vscode.workspace.workspaceFolders?.[0]);
+        let commandParam: string[] = config.get("mylua.misc.parameters") as [];
+        commandParam = ['-h'];
+        let command:string =  "E:\\ExtGit\\open.sources\\vscode-lua\\server\\bin\\lua-language-server.exe";
+        let serverOptions: ServerOptions = {
+            command: command,
+            args: commandParam,
+        };
+
+        this.client = new LanguageClient(
+            'mylua',
+            'mylua',
+            serverOptions,
+            clientOptions
+        );
+
+        //client.registerProposedFeatures();
+        await this.client.start();
+        this.onCommand();
+        this.statusBar();
+    }
+
+    async stop() {
+        this.client.stop();
+        for (const disposable of this.disposables) {
+            disposable.dispose();
+        }
+    }
+
+    statusBar() {
+        let client = this.client;
+        let bar = window.createStatusBarItem();
+        bar.text = 'mylua';
+        bar.command = 'mylua.statusBar';
+        this.disposables.push(Commands.registerCommand(bar.command, () => {
+            client.sendNotification('$/status/click');
+        }));
+        this.disposables.push(client.onNotification('$/status/show', (params) => {
+            bar.show();
+        }));
+        this.disposables.push(client.onNotification('$/status/hide', (params) => {
+            bar.hide();
+        }));
+        this.disposables.push(client.onNotification('$/status/report', (params) => {
+            bar.text = params.text;
+            bar.tooltip = params.tooltip;
+        }));
+        client.sendNotification('$/status/refresh');
+        this.disposables.push(bar);
+    }
+
+    onCommand() {
+        this.disposables.push(this.client.onNotification('$/command', (params) => {
+            Commands.executeCommand(params.command, params.data);
+        }));
+    }
+}
+
+let defaultClient: MyLuaClient;
+
+export function activate(context: ExtensionContext) {
+    registerCustomCommands(context);
+    function didOpenTextDocument(document: TextDocument) {
+        // We are only interested in language mode text
+        if (document.languageId !== 'mylua' || (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled')) {
+            return;
+        }
+
+        // Untitled files go to a default client.
+        if (!defaultClient) {
+            defaultClient = new MyLuaClient(context, [
+                { language: 'mylua' }
+            ]);
+            defaultClient.start();
+            return;
+        }
+    }
+
+    Workspace.onDidOpenTextDocument(didOpenTextDocument);
+    Workspace.textDocuments.forEach(didOpenTextDocument);
+    Workspace.onDidChangeWorkspaceFolders(() => {
+        if (defaultClient) {
+            defaultClient.stop();
+            defaultClient = new MyLuaClient(context, [
+                { language: 'mylua' }
+            ]);
+            defaultClient.start();
+        }
+    });
+}
+
+export async function deactivate() {
+    if (defaultClient) {
+        defaultClient.stop();
+        defaultClient = null as any;
+    }
+    return undefined;
+}
