951ff8497a18016c41a55e193be06d2e967c9f2f
onemore <909413016@qq.com>
2023-12-17 21:58:08 +0800

    实现语法高亮中-

---
+++ myserver/myserver/Model/MyLuaFile.cs
@@ -0,0 +1,39 @@
+﻿using MyServer.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServer.Model;
+public class MyLuaFile
+{
+    /// <summary>
+    /// 数据结构以后再重新设计。这次关注功能
+    /// </summary>
+    List<char[]> m_contents = new List<char[]>();
+
+    public void Init(string full_content)
+    {
+
+    }
+
+    public void Edit(TextDocContentChangeEvent textDocContentChangeEvent) {
+    
+    }
+
+    public void GetSymbolTree()
+    {
+
+    }
+
+    public void GetSyntaxTokens(Protocol.Range? range)
+    {
+
+    }
+
+    public void GetFolderTree()
+    {
+
+    }
+}
